317e72259916a819c2d84e989df4c5f2f02ea1a1
Paul Cullen Rowe <prowe214@gmail.com>
2015-07-27 11:46:05 -0600

    initial stripe API integration stuff

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('monk')(process.env.MONGOLAB_URI);
 var auctions = db.get('auctions');
+var stripe = require("stripe")(process.env.STRIPE_TEST_SECRET);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +17,8 @@ router.get('/auctions', function(req, res, next) {
 });
 
 router.get('/addauction', function(req, res, next) {
-  res.render('db/addauction');
+  console.log(req.session.id);
+  res.render('db/addauction', {title: 'Add an Auction', id: req.session.id});
 });
 
 router.post('/addauction', function(req, res, next) {
@@ -30,6 +32,23 @@ router.post('/addauction', function(req, res, next) {
 router.get('/logout', function(req, res, next) {
   req.session = null;
   res.redirect('/');
+});
+
+router.get('/:id/buy', function (req, res, next) {
+  auctions.findOne({_id: req.params.id}, function (err, doc) {
+    stripe.charges.create({
+      amount: doc.currentbid,
+      currency: 'usd',
+      customer: req.session.id,
+      }, function (err, charge) {
+        if (err) {
+          res.render('error');
+        } else {
+          res.render('thankyou');
+        }
+      }
+    );
+  });
 });
 
 router.get('/:id', function (req, res, next) {
